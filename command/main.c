1b16a23912b850a09bd03bdb84bf9e7b9bb54da1
KilianSen <kilian@senger.group>
2026-03-29 16:37:13 +0200

    Remove unused argument handling and output logic from main function

--- command/main.c
+++ command/main.c
@@ -2,11 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) return 0;
-    int count = atoi(argv[1]);
-    for (int i = 0; i < count && (i + 2) < argc; i++) {
-        printf("%s%s", argv[i+2], (i + 1 < count && i + 3 < argc) ? " " : "");
-    }
-    printf("\n");
+
     return 0;
 }
